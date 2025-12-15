20cc5ddf0d5a7d6b22caf923f8780dabed157679
JesseJohn7 <jessejohn260@gmail.com>
2025-12-15 09:37:16 +0100

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { SplitText } from "gsap/SplitText";
+import { ScrollTrigger ,SplitText } from "gsap/all";
+
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
